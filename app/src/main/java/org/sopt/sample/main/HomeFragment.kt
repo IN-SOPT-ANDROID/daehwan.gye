aca4eb55d680d9b027b19e61923491849d3e19b3
스프링 피바라기 <daehwan7989@naver.com>
2022-10-10 15:18:55 +0900

    Template Refactoring

--- app/src/main/java/org/sopt/sample/main/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/main/HomeFragment.kt
@@ -8,21 +8,20 @@ import org.sopt.sample.R
 import org.sopt.sample.common.FragmentTemplate
 import org.sopt.sample.databinding.FragmentHomeBinding
 
-class HomeFragment : FragmentTemplate() {
-    private var _binding: FragmentHomeBinding? = null
+class HomeFragment : FragmentTemplate<FragmentHomeBinding>() {
 
-    private val mockItemList = listOf<HomeListItem>(
-        HomeListItem.of(name = "CatchMe", body = "Nunu-Lee", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "FILL-IN", body = "daehwan.gye", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "hihi", body = "daehwan2yo", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "babo", body = "you", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "Hacker", body = "NUNU_LEE", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface ),
-        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface )
+    private val mockItemList = listOf(
+        HomeListItem.of(name = "CatchMe", body = "Nunu-Lee", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "FILL-IN", body = "daehwan.gye", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "hihi", body = "daehwan2yo", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "babo", body = "you", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "Hacker", body = "NUNU_LEE", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface),
+        HomeListItem.of(name = "BeMe", body = "Nunu-Lee", imageNo = R.drawable.myface)
     )
 
     override fun initBinding(
@@ -33,17 +32,13 @@ class HomeFragment : FragmentTemplate() {
         _binding = FragmentHomeBinding.inflate(inflater, container, attachToParrent)
     }
 
-    override fun getBinding() = requireNotNull(_binding)
-
-    override fun removeBinding() {
-        _binding = null;
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val homeListAdapter = HomeListAdapter(requireContext())
         homeListAdapter.setList(mockItemList)
         getBinding().rvHome.adapter = homeListAdapter
-
     }
+
+    override fun getBinding(): FragmentHomeBinding =
+        requireNotNull(_binding) as FragmentHomeBinding
 }
